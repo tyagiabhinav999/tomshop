8b1fadf3fc3dd54e64364af1af5b89bd50c24d86
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2023-12-17 21:10:07 +0530

    code required models for our tags app

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+class TaggedItem(models.Model):
+    # what tag is applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # to make our tag generic, we need two thing: (1) Type to find the table (of the object like product, video etc) (2) ID
+    object_id = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_object = GenericForeignKey()
+
+
