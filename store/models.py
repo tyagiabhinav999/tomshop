50127e9e960e898f5cea575f6c69fdc367c9bb4f
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2023-12-17 20:47:24 +0530

    code required models for our store app

--- store/models.py
+++ store/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, related_name='+') # Resolving CIRCULAR DEPENDENCY
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+    
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
@@ -28,6 +40,21 @@ class Order(models.Model):
     
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models):
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+class Cart(models):
+    created_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models):
+    quantity = models.PositiveSmallIntegerField()
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 class Address(models):
     street = models.CharField(max_length=255)
