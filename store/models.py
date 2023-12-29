00eeadeba8c159df2dec0c59aa180538a53495d6
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2023-12-29 15:17:08 +0530

    added membership choices in store customer model

--- store/models.py
+++ store/models.py
@@ -22,11 +22,21 @@ class Product(models.Model):
     promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+    (MEMBERSHIP_BRONZE, "Bronze"),
+    (MEMBERSHIP_SILVER, "Silver"),
+    (MEMBERSHIP_GOLD, "Gold")
+]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
