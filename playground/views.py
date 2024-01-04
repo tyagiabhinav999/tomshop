8ac4e368c74d9d0d2360410704daad06633bdbd4
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2024-01-04 19:39:30 +0530

    added templates, views and urls for filtering with Django ORM

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from store.models import Customer
 
 # Create your views here.
+def say_hello(request):
+    customers = Customer.objects.filter(email__icontains = '.com')
+    return render(request, 'hello.html', {'name': 'Abhinav', 'customers': list(customers)})
+
