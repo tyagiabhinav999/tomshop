e3e0a7d4758c60bb1a27ea26d2c7645251db8137
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2024-01-15 19:18:59 +0530

    added storefront as root endpoint and querying related fields

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from store.models import Customer
+from django.db.models import Q, F
+from store.models import Product, Collection, Order, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    customers = Customer.objects.filter(email__icontains = '.com')
-    return render(request, 'hello.html', {'name': 'Abhinav', 'customers': list(customers)})
+    # collections = Collection.objects.filter(featured_product__isnull = True)
+    orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # order_items = OrderItem.objects.select_related('order__customer')[:5] # .filter(inventory__lt = 20).filter(unit_price__lt = 20)
+    return render(request, 'hello.html', {'name': 'Abhinav', 'orders': list(orders)})
 
