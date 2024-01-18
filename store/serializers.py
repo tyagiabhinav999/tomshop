d119c12e16deab65fb39292433dce32b140a2df1
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2024-01-18 18:00:07 +0530

    serializing relationships way three using CollectionSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,12 +2,17 @@ from decimal import Decimal, ROUND_DOWN
 from rest_framework import serializers
 from .models import Product, Collection
 
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1).quantize(Decimal('0.00'), rounding=ROUND_DOWN)
