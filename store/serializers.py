94fdb0d7eb799a2674492549b8ac14b32067f1ff
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2024-01-18 18:26:13 +0530

    serializing relationships way four using hyperlink

--- store/serializers.py
+++ store/serializers.py
@@ -6,13 +6,16 @@ from .models import Product, Collection
 class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name = "collection-detail"
+    )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1).quantize(Decimal('0.00'), rounding=ROUND_DOWN)
--- store/views.py
+++ store/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
-# Create your views here.(
+# Create your views here.
 @api_view()
 def product_list(request):
     products = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(products, many=True)
+    serializer = ProductSerializer(products, many=True, context={"request": request})
     return Response(serializer.data)
 
 
@@ -16,4 +16,18 @@ def product_list(request):
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_list(request):
+    collection = Collection.objects.all()
+    serializer = CollectionSerializer(collection, many=True)
     return Response(serializer.data)
