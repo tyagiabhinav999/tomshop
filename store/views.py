11d25b40fb2352d075cf46a4f1fd28ecea636510
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2024-01-24 18:25:13 +0530

    implementing viewsets and routing

--- store/views.py
+++ store/views.py
@@ -1,40 +1,32 @@
 from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {"request": self.request}
-    
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
+
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitem_set.count() > 0:
             return Response({"error":"Product can't be deleted."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class CollectionList(ListCreateAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('product')).all()
-    serializer_class = CollectionSerializer
     
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('product'))
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(products_count=Count('product')).all()
     serializer_class = CollectionSerializer
 
     def delete(self, request, pk):
