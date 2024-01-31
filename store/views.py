0b90565c59c1c41d961e4b6bc4dd66a455f68e4f
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2024-01-31 15:43:34 +0530

    Implement SearchFilter, SortFilter and pagination

--- store/views.py
+++ store/views.py
@@ -4,9 +4,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .filters import ProductFilter
+from .pagination import DefaultPagination
 
 # Create your views here.
 
@@ -14,8 +16,11 @@ from .filters import ProductFilter
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer 
-    filter_backends = [DjangoFilterBackend]
+    pagination_class = DefaultPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {"request": self.request}
